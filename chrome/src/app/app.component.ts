43e5f25e92e70e3f38d0f9baac492b7dde9f171f
Scott Ryan <scott@theryansplace.com>
2020-09-21 15:51:11 -0600

    update to add content

--- chrome/src/app/app.component.ts
+++ chrome/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chrome';
+export class AppComponent implements OnInit {
+  isLoggedIn = false;
+  processing = false;
+  loginForm;
+
+  constructor(private fb: FormBuilder){
+    this.loginForm = fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  get f(): any{
+    return this.loginForm.controls;
+  }
+
+  ngOnInit(): void{
+    // Check for login creds
+  }
+
+  executeLogin(): void{
+  }
 }
--- chrome/src/app/app.module.ts
+++ chrome/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
